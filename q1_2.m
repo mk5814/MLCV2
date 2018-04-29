e8115bb3b2c99ebfbb2c7fee87a4a916a0742874
mk5814 <mert.kaya14@imperial.ac.uk>
2018-04-29 22:33:44 +0100

    Update q1_2.m

--- q1_2.m
+++ q1_2.m
@@ -19,13 +19,16 @@ qual = 0.01;
 poi1 = myHarris(I1,qual);
 poi2 = myHarris(I2,qual);
 
-[f1, validpts1] = extractFeatures(I1, poi1);
-[f2, validpts2] = extractFeatures(I2, poi2);
+% [f1, validpts1] = extractFeatures(I1, poi1);
+% [f2, validpts2] = extractFeatures(I2, poi2);
 
-map = nnMatch(f1, f2, 0.6);
+descriptors1 = descriptorExtract(I1, poi1);
+descriptors2 = descriptorExtract(I2, poi2);
 
-matchedPoints1 = validpts1(map(:,1),:);
-matchedPoints2 = validpts2(map(:,2),:);
+map = nnMatch(descriptors1, descriptors2, 0.6);
+
+matchedPoints1 = poi1(map(:,1),:);
+matchedPoints2 = poi2(map(:,2),:);
 figure;
 showMatchedFeatures(I1,I2,matchedPoints1,matchedPoints2);
 
